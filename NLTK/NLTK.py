5d6c32fd59809797ea2361401a937babc4a8e9a9
Paulo <pauloh_2015@yahoo.com>
2023-03-08 23:11:07 -0300

    Create NLTK.py

---
+++ NLTK/NLTK.py
@@ -0,0 +1,5 @@
+#Processamento de linguagem Natural é o uso de bibliotecas de programação para que computadores absorvam conteúdos de dados não estrutudaso que seriam humanamente impossíveis e os resumam para algo mais paupável. Ex.: utilizar NLTK para resumir milhares de comentários em um produto em reviews positivos e negativos.
+
+#NLTK = Natural Language Tool Kit. Utilizado geralmente na parte de pré-processamento de dados.
+
+#Etapas de um pré-procesasmento
